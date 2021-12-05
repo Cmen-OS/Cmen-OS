eec855e510c48111862be91dd6bcbb6b521a7fce
LaBalaElmo <jrttaquichiri@gmail.com>
2021-12-05 18:27:01 -0400

    actualizado

--- db/urls.py
+++ db/urls.py
@@ -3,6 +3,7 @@ from db import views
 
 urlpatterns = [
     url(r'^login$', views.login),
+    url(r'^admin$', views.admin),
     url(r'^registro$', views.registro),
     url(r'^animal$', views.animal),
     url(r'^operador$', views.operador),
