460dc1fe9ed071f40fe125e913c81bfc951b2eb6
LaBalaElmo <jrttaquichiri@gmail.com>
2021-12-04 22:22:55 -0400

    Funcionalidad

---
+++ src/app/services/operador/operador.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { Operador } from '../../models/operador/operador.model';
+
+const baseUrl = 'http://localhost:8080'
+@Injectable({
+  providedIn: 'root'
+})
+export class OperadorService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Operador[]> {
+    return this.http.get<Operador[]>(baseUrl);
+  }
+
+  get(id: any): Observable<Operador> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  findByPassword(email: any): Observable<Operador[]> {
+    return this.http.get<Operador[]>(`${baseUrl}?email=${email}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(`${baseUrl}/login`,data)
+  }
+}
