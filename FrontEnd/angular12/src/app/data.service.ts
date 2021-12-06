5b2f358e4841115e6462051f334d83543e9cdfdd
H0OE <rodrigoquiroz308@gmail.com>
2021-12-06 01:17:01 -0400

    reconocimiento de usuario header

---
+++ FrontEnd/angular12/src/app/data.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  counter = 1;
+  count: BehaviorSubject<number>;
+
+
+
+
+
+  constructor() {
+
+    this.count = new BehaviorSubject(this.counter);
+
+
+  }
+
+  nextCount() {
+    this.counter++
+
+    this.count.next(this.counter);
+
+
+  }
+
+
+
+
+
+
+}
