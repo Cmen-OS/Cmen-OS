ad0ef69925a3931d843d1b2b7b9225dbb9c65efb
H0OE <rodrigoquiroz308@gmail.com>
2021-12-14 22:28:11 -0400

    pagina taxonomia, microchip

--- FrontEnd/angular12/src/app/screens/microchip/microchip.component.ts
+++ FrontEnd/angular12/src/app/screens/microchip/microchip.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators, FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-microchip',
@@ -6,10 +7,82 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./microchip.component.css']
 })
 export class MicrochipComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(    private formBuilder: FormBuilder,
+  ) {
+    this.buildForm();
+
+  }
 
   ngOnInit(): void {
   }
 
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      numMicrochip: ['', Validators.required],
+      tamanio: ['', Validators.required],
+      peso: ['', Validators.required],
+      caracteristicasFenotipicas: ['', Validators.required],
+
+      rabia: ['', Validators.required],
+      desparacitacion: [''],
+      otros: ['', Validators.required],
+      tiposDesparacitacion: ['', Validators.required],
+
+      observaciones: ['', Validators.required],
+      encargado: ['', Validators.required],
+
+      nombre: ['', Validators.required],
+      especie: ['', Validators.required],
+      nombreCientifico: ['', Validators.required],
+      edad: ['', Validators.required],
+      sexo: ['', Validators.required],
+
+
+    });
+  }
+
+
+  isBoxValid(box: String): Boolean {
+    // @ts-ignore
+    return this.form.get(box).touched && this.form.get(box).hasError('required');
+  }
+
+  isRabia: string[] = [
+    'si',
+    'no',
+    'Otro'
+  ]
+
+  isDesparatizacion: string[] = [
+    'si',
+    'no',
+    'Otro'
+  ]
+
+  desparazitaciones: string[] = [
+    'Parasitos',
+    'Gusanos',
+    'Papagallos'
+  ]
+
+  sexos: string[] = [
+    'Masculino',
+    'Femenino',
+    'otro'
+  ]
+
+
+  save(event: Event) {
+    if (this.form.valid ) {
+      console.log(this.form.value);
+
+
+
+
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
 }
--- FrontEnd/angular12/src/app/screens/taxonomia/taxonomia.component.ts
+++ FrontEnd/angular12/src/app/screens/taxonomia/taxonomia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators, FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-taxonomia',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./taxonomia.component.css']
 })
 export class TaxonomiaComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(    private formBuilder: FormBuilder,
+  ) {
+    this.buildForm();
+
+  }
 
   ngOnInit(): void {
   }
 
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      orden: ['', Validators.required],
+      familia: ['', Validators.required],
+      genero: ['', Validators.required],
+      especie: ['', Validators.required],
+      subespecie: ['', Validators.required],
+
+
+
+    });
+  }
+
+  isBoxValid(box: String): Boolean {
+    // @ts-ignore
+    return this.form.get(box).touched && this.form.get(box).hasError('required');
+  }
+
+  save(event: Event) {
+    if (this.form.valid ) {
+      console.log(this.form.value);
+
+
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  modificar() {
+    //boton modifica
+  }
+
+  baja() {
+    //boton dar de baja
+  }
 }
